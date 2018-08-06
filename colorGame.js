8cb6ef8ccd347f816e1d827ff466c0b59d1faa41
Oleksii Mostovyi <oleksii.mostovyi@globallogic.com>
2018-08-06 09:43:33 +0300

    basic logic done. need refactoring

--- colorGame.js
+++ colorGame.js
@@ -1,4 +1,5 @@
-var colors = colorsSet(6);
+var numSquares = 6;
+var colors = colorsSet(numSquares);
 var squares = document.querySelectorAll('.square');
 var pickedColor = colors[randNum(colors.length)];
 var pickedDispColor = document.querySelector('h1 span');
@@ -8,22 +9,58 @@ var easyBtn = document.querySelector('#easy');
 var heardBtn = document.querySelector('#heard');
 var h1 = document.querySelector('h1')
 
-
+// default state
+function defaultState () {
+    displayMessage.textContent = '';
+    resetBtn.textContent = 'New Colors';
+    heardBtn.classList.add('selected');
+    easyBtn.classList.remove('selected');
+    pickedDispColor.textContent = pickedColor;
+}
+defaultState();
+// buttons
+resetBtn.addEventListener('click' ,function () {
+    colors = colorsSet(numSquares);
+    pickedColor = colors[randNum(colors.length)];
+    pickedDispColor.textContent = pickedColor;
+    setColors();
+    displayMessage.textContent = '';
+    resetBtn.textContent = 'New Colors';
+});
 
 easyBtn.addEventListener('click', function () {
-    for (let i = 0; i < colors.length; i++) {
-        
+    heardBtn.classList.remove('selected');
+    easyBtn.classList.add('selected');
+    displayMessage.textContent = '';
+    resetBtn.textContent = 'New Colors';
+    numSquares = 3;
+    colors = colorsSet(numSquares);
+    pickedColor = colors[randNum(colors.length)];
+    pickedDispColor.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.background = colors[i];
+        } else {
+            squares[i].style.display = 'none';
+        }
     }
 });
+
 heardBtn.addEventListener('click', function () {
-
-});
-
-resetBtn.addEventListener('click' ,function () {
-    defaultState();
-});
-
-
+    heardBtn.classList.add('selected');
+    easyBtn.classList.remove('selected');
+    displayMessage.textContent = '';
+    resetBtn.textContent = 'New Colors';
+    numSquares = 6;
+    colors = colorsSet(numSquares);
+    pickedColor = colors[randNum(colors.length)];
+    pickedDispColor.textContent = pickedColor;
+    for (let i = 0; i < colors.length; i++) {
+        squares[i].style.background = colors[i];
+        squares[i].style.display = 'block';
+    }
+})
+// squares
 for (let i = 0; i < squares.length; i++) {
     setColors();
     // add event listeners to squares
@@ -40,15 +77,6 @@ for (let i = 0; i < squares.length; i++) {
             this.style.backgroundColor = 'rgb(38, 38, 38)';
         }
     })
-}
-// set default settings for the game
-function defaultState() {
-    colors = colorsSet(6);
-    pickedColor = colors[randNum(colors.length)];
-    pickedDispColor.textContent = pickedColor;
-    setColors();
-    displayMessage.textContent = '';
-    resetBtn.textContent = 'New Colors';
 }
 // sets square's colors
 function setColors() {
@@ -80,7 +108,7 @@ function colorOne() {
 function colorsSet(num) {
     let arr = [];
     for (let i=0; i < num; i++) {
-        arr.push(colorOne());
+        arr.push(colorOne(numSquares));
     }
     return arr;
 }
